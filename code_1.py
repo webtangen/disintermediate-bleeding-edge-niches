1680b45223641248aafe26c3a23e5fee7213af7e
webtangen <143550134+webtangen@users.noreply.github.com>
2024-01-28 23:25:40 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,53 @@
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = generateRandomData();
+    }
+    data.forEach(item => {
+
+    });
+
+    for (let i = 0; i < 10; i++) {
+function main() {
+    const data = [];
+
+function generateRandomData() {
+    return data;
+}
+main();
+        console.log("Random Number: " + item);
+}            data[i] = (int) (Math.random() * 100) + 1;
+        for (int item : data) {
+        for (int i = 0; i < 10; i++) {
+public class RandomDataGenerator {
+        }
+            System.out.println("Random Number: " + item);
+}
+
+        int[] data = new int[10];
+        }
+
+    public static void main(String[] args) {
+    }    }
+        for (int i = 0; i < 10; i++) {
+        }
+        int[] data = new int[10];
+        for (int item : data) {
+    public static void main(String[] args) {
+public class RandomDataGenerator {
+        }
+}
+
+
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);            data[i] = (int) (Math.random() * 100) + 1;
+public class RandomDataGenerator {
+}
+
+        for (int i = 0; i < 10; i++) {
+
+        }
+            System.out.println("Random Number: " + item);
+        int[] data = new int[10];
+        }
+    }
+        for (int item : data) {
+    public static void main(String[] args) {
